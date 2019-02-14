4c60f162c8b70ca5e4a1a51594504c89bac84f55
kapa <kapa16@inbox.ru>
2019-02-14 23:28:03 +0500

    setup django

--- messanger/messanger/urls.py
+++ messanger/messanger/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import Messanger
 
 urlpatterns = [
+    path('', Messanger.as_view()),
     path('admin/', admin.site.urls),
 ]
