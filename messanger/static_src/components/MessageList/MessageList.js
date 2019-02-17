44fb78ddf9c0c1d25bfbc3a8153ed1b591077eb0
kapa <kapa16@inbox.ru>
2019-02-17 23:57:41 +0500

    message list create

--- messanger/static_src/components/MessageList/MessageList.js
+++ messanger/static_src/components/MessageList/MessageList.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import MessageItem from "../MessageItem/MessageItem";
+import './MessageList.sass';
 
-const MessageList = () => {
+
+const MessageList = ({ messages }) => {
+
+  const messagesItems = messages.map(({ id, ...msgProps }) => {
+    let className = 'list__item';
+    if (msgProps.sender === 'mySelf') {
+      className += ' list__item-my';
+    } else {
+      className += ' list__item-other';
+    }
+
+    return (
+      <li
+        key={id}
+        className={className}
+      >
+        <MessageItem
+          { ...msgProps }
+        />
+      </li>
+    );
+  });
+
   return (
-    <div>
-
-    </div> 
+    <ul className="list">
+      { messagesItems }
+    </ul>
   )
 };
 
