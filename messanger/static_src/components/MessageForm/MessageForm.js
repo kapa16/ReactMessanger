376eadc3f1cb2edf62b27f8429c56c5b0d6c43cc
kapa <kapa16@inbox.ru>
2019-02-17 23:58:04 +0500

    message form correct

--- messanger/static_src/components/MessageForm/MessageForm.js
+++ messanger/static_src/components/MessageForm/MessageForm.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-const MessageForm = () => {
+const MessageForm = ({ createMessageHandler, onChangeMessage, message }) => {
+
+
+
   return (
     <div>
-      <form action="#" name="msgForm">
-        <input type="text" name="msgInput" placeholder="Write message"/>
-        <button type="submit"></button>
+      <form action="#" name="msgForm" onSubmit={(e) => createMessageHandler(e)}>
+        <input type="text" name="msgInput" placeholder="Write message"
+               onChange={(e) => onChangeMessage(e)}
+               value={message}
+        />
+        <button type="submit">Send</button>
       </form>
     </div>
   )
