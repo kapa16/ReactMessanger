8121599ca34bdfe3f4d0d3d1bd06ac285b66f6f8
kapa <kapa16@inbox.ru>
2019-02-17 17:40:48 +0500

    add message form component

---
+++ messanger/static_src/components/MessageForm/MessageForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MessageForm = () => {
+  return (
+    <div>
+      <form action="#" name="msgForm">
+        <input type="text" name="msgInput" placeholder="Write message"/>
+        <button type="submit" className={}></button>
+      </form>
+    </div>
+  )
+};
+
+export default MessageForm;
