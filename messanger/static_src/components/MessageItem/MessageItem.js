9a352095a88617cb938670614f8e40e61c22174b
kapa16 <kapa16@inbox.ru>
2019-02-17 21:03:46 +0500

    create message item

---
+++ messanger/static_src/components/MessageItem/MessageItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MessageItem = () => {
+  return (
+    <div>
+      <div>Message content</div>
+      <div>12:26</div>
+
+    </div>
+  )
+};
+
+export default MessageItem;
