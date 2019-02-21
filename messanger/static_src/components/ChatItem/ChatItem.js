f7f3c3c3716b852f68818a2a13aed87da0495eea
kapa16 <kapa16@inbox.ru>
2019-02-21 19:44:38 +0500

    add prop types, mark selected chat, add Link

--- messanger/static_src/components/ChatItem/ChatItem.js
+++ messanger/static_src/components/ChatItem/ChatItem.js
@@ -1,24 +1,31 @@
 import React, {Fragment} from 'react';
+import PropTypes from 'prop-types';
 import {ListItem} from "@material-ui/core";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
+import {Link} from "react-router-dom";
 
-const ChatItem = ({id, title}) => {
-
-  const ListItemLink = (props) => <ListItem button component="a" {...props}/>;
-
-
+const ChatItem = ({selected, id, title}) => {
   return (
     <Fragment>
-      <ListItemLink href={`/chat/${id}`}>
-        <ListItemText>
-          {title}
-        </ListItemText>
-      </ListItemLink>
+      <ListItem
+        selected={selected}
+      >
+        <Link to={`/chat/${id}`}>
+          <ListItemText>
+            {title}
+          </ListItemText>
+        </Link>
+      </ListItem>
       <Divider/>
     </Fragment>
-
   )
+};
+
+ChatItem.propTypes ={
+  selected: PropTypes.bool,
+  id: PropTypes.number,
+  title: PropTypes.string
 };
 
 export default ChatItem;
