cb1f2be53c81642a5fb5704f0be565fbd9fca725
kapa <kapa16@inbox.ru>
2019-02-21 18:29:24 +0500

    delete handler

--- messanger/static_src/components/Header/Header.js
+++ messanger/static_src/components/Header/Header.js
@@ -4,14 +4,13 @@ import MenuIcon from '@material-ui/icons/Menu';
 import Grid from "@material-ui/core/Grid";
 import './Header.sass'
 
-const Header = ({handleChatListOpen}) => {
+const Header = () => {
   return (
     <AppBar position="static">
         <Toolbar disableGutters className="container">
           <IconButton
             aria-label="Menu"
             color="inherit"
-            onClick={handleChatListOpen}
           >
             <MenuIcon />
           </IconButton>
