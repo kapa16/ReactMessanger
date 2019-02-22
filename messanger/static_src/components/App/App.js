ff403c659e8b0fcebc0b6c195f21898909ed7517
Pavel <kapa16@inbox.ru>
2019-02-22 11:05:19 +0500

    add images

--- messanger/static_src/components/App/App.js
+++ messanger/static_src/components/App/App.js
@@ -9,21 +9,25 @@ const chats = [
   {
     id: 1,
     title: "First chat",
+    img: "../../img/1.jpg",
     messages: []
   },
   {
     id: 2,
     title: "Second chat",
+    img: "../../img/2.jpg",
     messages: []
   },
   {
     id: 3,
     title: "Third chat",
+    img: "",
     messages: []
   },
   {
     id: 4,
     title: "Fourth chat",
+    img: "",
     messages: []
   },
 ];
