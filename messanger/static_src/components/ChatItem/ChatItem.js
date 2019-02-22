e4c0e485e130d99278e763850eca5876bf32e3b2
Pavel <kapa16@inbox.ru>
2019-02-22 11:05:41 +0500

    add avatar for list item

--- messanger/static_src/components/ChatItem/ChatItem.js
+++ messanger/static_src/components/ChatItem/ChatItem.js
@@ -4,16 +4,22 @@ import PropTypes from 'prop-types';
 import {ListItem} from "@material-ui/core";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+
 
 const ListItemLink = ({to, ...props}) => (
   <ListItem button component={Link} to={to} {...props}/>
   )
 ;
 
-const ChatItem = ({selected, id, title}) => {
+const ChatItem = ({selected, id, title, img}) => {
   return (
     <Fragment>
         <ListItemLink to={`/chat/${id}`} selected={selected}>
+          <ListItemAvatar>
+            <Avatar src={img}/>
+          </ListItemAvatar>
           <ListItemText>
             {title}
           </ListItemText>
