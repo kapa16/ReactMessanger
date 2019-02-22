f4485fda07c0246d1ddee364dea8d734ed7aecf2
kapa <kapa16@inbox.ru>
2019-02-22 08:31:08 +0500

    change button to material-ui

--- messanger/static_src/components/MessageForm/MessageForm.js
+++ messanger/static_src/components/MessageForm/MessageForm.js
@@ -1,6 +1,8 @@
 import React, {Fragment} from 'react';
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 import Input from "@material-ui/core/Input";
+import IconButton from "@material-ui/core/IconButton";
+import SendIcon from '@material-ui/icons/Send';
 import './MessageForm.sass'
 
 const MessageForm = ({createMessageHandler, onChangeMessage, currentMessage}) => {
@@ -20,7 +22,9 @@ const MessageForm = ({createMessageHandler, onChangeMessage, currentMessage}) =>
           value={currentMessage}
           className="msg-form__input"
         />
-        <button type="submit" className="msg-form__button"></button>
+        <IconButton type="submit" aria-label="Send" color="inherit">
+          <SendIcon />
+        </IconButton>
       </form>
     </Fragment>
   )
