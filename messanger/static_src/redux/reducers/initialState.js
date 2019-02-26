24370de0e09dd860dadf4154ba9bd6feb8800c0c
Pavel <kapa16@inbox.ru>
2019-02-26 12:18:33 +0500

    initialization store for redux

---
+++ messanger/static_src/redux/reducers/initialState.js
@@ -0,0 +1,32 @@
+const messageInitialState = {
+  currentMessage: '',
+  robotMessage: 'This is robot answer',
+  chats: [
+    {
+      id: 1,
+      title: "First chat",
+      img: "../../img/1.jpg",
+      messages: []
+    },
+    {
+      id: 2,
+      title: "Second chat",
+      img: "../../img/2.jpg",
+      messages: []
+    },
+    {
+      id: 3,
+      title: "Third chat",
+      img: "",
+      messages: []
+    },
+    {
+      id: 4,
+      title: "Fourth chat",
+      img: "",
+      messages: []
+    },
+  ]
+};
+
+export default messageInitialState;
