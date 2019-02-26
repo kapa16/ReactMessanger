949b67c720cd78ebb8df296bf859e8c154273ad1
Pavel <kapa16@inbox.ru>
2019-02-26 17:58:48 +0500

    add actions for change chat & robot message

--- messanger/static_src/redux/actions/messagesActions.js
+++ messanger/static_src/redux/actions/messagesActions.js
@@ -1,3 +1,4 @@
-export const SEND_MESSAGE = 'SEND_MESSAGE';
-
 export const INPUT_MESSAGE = 'INPUT_MESSAGE';
+export const SEND_MESSAGE = 'SEND_MESSAGE';
+export const SEND_ROBOT_MESSAGE = 'SEND_ROBOT_MESSAGE';
+export const CHANGE_CHAT_ID = 'CHANGE_CHAT_ID';
--- messanger/static_src/redux/reducers/messageReducer.js
+++ messanger/static_src/redux/reducers/messageReducer.js
@@ -1,10 +1,13 @@
-import messageInitialState from "./initialState";
-import {INPUT_MESSAGE, SEND_MESSAGE} from '../actions/messagesActions'
+import update from 'immutability-helper';
+import initialState from "./initialState";
+import {CHANGE_CHAT_ID, INPUT_MESSAGE, SEND_MESSAGE, SEND_ROBOT_MESSAGE} from '../actions/messagesActions'
+
+let currentMessageId = 1;
 
 const createMessage = (text, sender) => {
   const created = new Date();
   return {
-    id: this.currentMessageId++,
+    id: currentMessageId++,
     text,
     sender,
     createdDate: created.toLocaleDateString(),
@@ -12,16 +15,39 @@ const createMessage = (text, sender) => {
   }
 };
 
-const messageReducer = (state = messageInitialState, action) => {
+const getNewState = (chats, currentMessage, sender, currentChatId) => {
+  const currentChat = chats.find((chat) => chat.id === currentChatId);
+  if (!currentChat) {
+    return {};
+  }
 
+  currentChat.messages.push(createMessage(currentMessage, sender));
+
+  const index = chats.indexOf(currentChat);
+  return {
+    chats: [
+      ...chats.slice(0, index),
+      Object.assign({}, currentChat),
+      ...chats.slice(index + 1)
+    ],
+    currentMessage: ''
+  }
+};
+
+
+const messageReducer = (state = initialState.messageInitialState, action) => {
   switch (action.type) {
     case SEND_MESSAGE:
-      const { messageText, sender } = action.payload;
-      createMessage(messageText, sender);
+      return {...state, ...getNewState(state.chats, state.currentMessage, 'mySelf', state.currentChatId)};
+
+    case SEND_ROBOT_MESSAGE:
+      return state;
 
     case INPUT_MESSAGE:
-      const { input } = action.payload;
-      return { input };
+      return {...state, ...{currentMessage: action.payload}};
+
+    case CHANGE_CHAT_ID:
+      return {...state, ...{currentChatId: action.payload}};
   }
   return state;
 };
