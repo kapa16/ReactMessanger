5f287d68a77126df75f0b18389fd0c820b532c72
Pavel <kapa16@inbox.ru>
2019-02-26 12:18:04 +0500

    actions: input message for change input field sende messege will create and add message to store

--- messanger/static_src/redux/actionsCreator/messageActionsCreator.js
+++ messanger/static_src/redux/actionsCreator/messageActionsCreator.js
@@ -1,8 +1,20 @@
-const sendMessage = () => {
+import {INPUT_MESSAGE, SEND_MESSAGE} from "../actions/messagesActions";
+
+export const sendMessage = (messageText, sender) => {
   return {
     type: SEND_MESSAGE,
-
+    payload: {
+      messageText,
+      sender
+    }
   }
 };
 
-export sendMessage;
+export const inputMessage = (input) => {
+  return {
+    type: INPUT_MESSAGE,
+    payload: input
+  }
+};
+
+
