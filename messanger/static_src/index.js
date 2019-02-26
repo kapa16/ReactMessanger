fc29e57e3909054a810f4ba931a6147d5d9579af
Pavel <kapa16@inbox.ru>
2019-02-26 12:19:23 +0500

    add redux to application & create store

--- messanger/static_src/index.js
+++ messanger/static_src/index.js
@@ -3,10 +3,18 @@ import ReactDom from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import App from "./components/App/App";
 import "./css/main.sass"
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import rootReducer from "./redux/reducers";
+
+const store = createStore(rootReducer);
 
 ReactDom.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+  ,
   document.getElementById('root')
 );
