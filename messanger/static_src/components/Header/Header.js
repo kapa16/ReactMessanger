46a442df4c5e67aea507222704b601ac3604918c
kapa16 <kapa16@inbox.ru>
2019-03-02 16:38:02 +0500

    create component header menu

--- messanger/static_src/components/Header/Header.js
+++ messanger/static_src/components/Header/Header.js
@@ -1,71 +1,56 @@
 import React, { Component } from 'react';
 import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
-import './Header.sass'
-import Menu from "@material-ui/core/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
 import NewChatForm from "../NewChatForm/NewChatForm";
-import { bindActionCreators } from "redux";
-import { openAddChatMenu, openCloseMenu } from "../../redux/actionsCreator/messageActionsCreator";
-import { connect } from "react-redux";
+import HeaderMenu from "../HeaderMenu/HeaderMenu";
 
-const Header = ({openMenu, openCloseMenu, openAddChatMenu}) => {
+class Header extends Component {
 
-  let anchorElMenu = null;
-
-  const setAnchor = (evt) => {
-    anchorElMenu = evt.currentTarget;
-    console.log(anchorElMenu);
+  state = {
+    anchorEl: null
   };
 
-  return (
-    <AppBar position="static">
-      <Toolbar disableGutters className="container">
-        <IconButton
-          aria-label="Menu"
-          color="inherit"
-          aria-owns={openMenu ? 'simple-menu' : undefined}
-          aria-haspopup="true"
-          onClick={(evt) => {
-            setAnchor(evt);
-            console.log(anchorElMenu);
-            openCloseMenu()
-          }}
-        >
-          <MenuIcon/>
-        </IconButton>
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
 
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorElMenu}
-          open={openMenu}
-          onClose={() => openCloseMenu()}
-        >
-          <MenuItem onClick={() => openCloseMenu()}>Profile</MenuItem>
-          <MenuItem onClick={() => {
-            openAddChatMenu();
-            openCloseMenu()
-          }}>Add chat</MenuItem>
-          <MenuItem onClick={() => openCloseMenu()}>Logout</MenuItem>
-        </Menu>
-        <Typography variant="h6" color="inherit">
-          Messanger
-        </Typography>
-      </Toolbar>
-      <NewChatForm/>
-    </AppBar>
-  )
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
 
-};
+  render() {
+    const { anchorEl } = this.state;
 
-const mapStateToProps = (state) => {
-  const {openMenu} = state.messageReducer;
-  return {
-    openMenu: openMenu,
+    return (
+      <AppBar position="static">
+        <Toolbar disableGutters className="container">
+          <IconButton
+            aria-label="Menu"
+            color="inherit"
+            aria-owns={anchorEl ? 'simple-menu' : undefined}
+            aria-haspopup="true"
+            onClick={this.handleClick}
+          >
+            <MenuIcon/>
+          </IconButton>
+
+          <HeaderMenu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            handleCloseMenu={this.handleClose}
+            open={Boolean(anchorEl)}
+          />
+
+          <Typography variant="h6" color="inherit">
+            Messanger
+          </Typography>
+        </Toolbar>
+
+        <NewChatForm/>
+
+      </AppBar>
+    );
   }
-};
+}
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators({openCloseMenu, openAddChatMenu}, dispatch);
-
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default (Header);
